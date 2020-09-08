21f4b194db437f28064bea8114d9cfcf45720120
Hajime Fukuda <hajime.fukuda@me.com>
2020-09-08 18:32:56 +0900

    added merge as a command

--- src/bin/merge.rs
+++ src/bin/merge.rs
@@ -1,29 +1,21 @@
 use diffmerge::merge;
+use std::env;
+use std::fs::read;
 
-fn main() {
-        println!(
-                "{}",
-                merge(
-                        "a
-b
-c
-d
-e
-k
-f",
-                        "a
-c
-d
-e1
-k
-fz",
-                        "a
-c
-d
-e2
-k
-fz
-"
-                )
-        );
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        println!("Usage {} <ANCESTOR> <BRANCH1> <BRANCH2>", &args[0]);
+        return Ok(());
+    }
+
+    let ancestor = String::from_utf8(read(&args[1])?)?;
+    let branch1 = String::from_utf8(read(&args[2])?)?;
+    let branch2 = String::from_utf8(read(&args[3])?)?;
+    let mut merge = merge(&ancestor, &branch1, &branch2);
+    merge.set_names(&args[2], &args[3]);
+
+    println!("{}", merge);
+
+    Ok(())
 }
